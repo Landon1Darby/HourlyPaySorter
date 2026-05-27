5e343ee3277fe19f2817decddf15db1c484381fc
Landon Darby <kelsodarby732@gmail.com>
2026-05-26 23:31:54 -0700

    Add PaySorter project

---
+++ PaySorter lab #3/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace PaySorter_lab__3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.personelDataSet = new PaySorter_lab__3.PersonelDataSet();
+            this.tableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tableTableAdapter = new PaySorter_lab__3.PersonelDataSetTableAdapters.TableTableAdapter();
+            this.employeeIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.positionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hourlyPayRateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SortAscending = new System.Windows.Forms.Button();
+            this.Decending = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personelDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.employeeIDDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.positionDataGridViewTextBoxColumn,
+            this.hourlyPayRateDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tableBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(734, 203);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // personelDataSet
+            // 
+            this.personelDataSet.DataSetName = "PersonelDataSet";
+            this.personelDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tableBindingSource
+            // 
+            this.tableBindingSource.DataMember = "Table";
+            this.tableBindingSource.DataSource = this.personelDataSet;
+            // 
+            // tableTableAdapter
+            // 
+            this.tableTableAdapter.ClearBeforeFill = true;
+            // 
+            // employeeIDDataGridViewTextBoxColumn
+            // 
+            this.employeeIDDataGridViewTextBoxColumn.DataPropertyName = "EmployeeID";
+            this.employeeIDDataGridViewTextBoxColumn.HeaderText = "EmployeeID";
+            this.employeeIDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.employeeIDDataGridViewTextBoxColumn.Name = "employeeIDDataGridViewTextBoxColumn";
+            this.employeeIDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.employeeIDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // positionDataGridViewTextBoxColumn
+            // 
+            this.positionDataGridViewTextBoxColumn.DataPropertyName = "Position";
+            this.positionDataGridViewTextBoxColumn.HeaderText = "Position";
+            this.positionDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.positionDataGridViewTextBoxColumn.Name = "positionDataGridViewTextBoxColumn";
+            this.positionDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // hourlyPayRateDataGridViewTextBoxColumn
+            // 
+            this.hourlyPayRateDataGridViewTextBoxColumn.DataPropertyName = "HourlyPayRate";
+            this.hourlyPayRateDataGridViewTextBoxColumn.HeaderText = "HourlyPayRate";
+            this.hourlyPayRateDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.hourlyPayRateDataGridViewTextBoxColumn.Name = "hourlyPayRateDataGridViewTextBoxColumn";
+            this.hourlyPayRateDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // SortAscending
+            // 
+            this.SortAscending.Location = new System.Drawing.Point(171, 324);
+            this.SortAscending.Name = "SortAscending";
+            this.SortAscending.Size = new System.Drawing.Size(133, 41);
+            this.SortAscending.TabIndex = 1;
+            this.SortAscending.Text = "Sort Ascending";
+            this.SortAscending.UseVisualStyleBackColor = true;
+            this.SortAscending.Click += new System.EventHandler(this.SortAscending_Click);
+            // 
+            // Decending
+            // 
+            this.Decending.Location = new System.Drawing.Point(421, 324);
+            this.Decending.Name = "Decending";
+            this.Decending.Size = new System.Drawing.Size(133, 41);
+            this.Decending.TabIndex = 2;
+            this.Decending.Text = "Sort Decending";
+            this.Decending.UseVisualStyleBackColor = true;
+            this.Decending.Click += new System.EventHandler(this.Decending_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Decending);
+            this.Controls.Add(this.SortAscending);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.personelDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private PersonelDataSet personelDataSet;
+        private System.Windows.Forms.BindingSource tableBindingSource;
+        private PersonelDataSetTableAdapters.TableTableAdapter tableTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn employeeIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn positionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hourlyPayRateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button SortAscending;
+        private System.Windows.Forms.Button Decending;
+    }
+}
+
---
+++ PaySorter lab #3/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaySorter_lab__3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'personelDataSet.Table' table. You can move, or remove it, as needed.
+            this.tableTableAdapter.Fill(this.personelDataSet.Table);
+
+        }
+
+        private void SortAscending_Click(object sender, EventArgs e)
+        {
+            this.tableTableAdapter.Fill(this.personelDataSet.Table);
+            dataGridView1.Sort(dataGridView1.Columns["hourlyPayRateDataGridViewTextBoxColumn"], System.ComponentModel.ListSortDirection.Ascending);
+        }
+
+        private void Decending_Click(object sender, EventArgs e)
+        {
+            this.tableTableAdapter.Fill(this.personelDataSet.Table);
+            dataGridView1.Sort(dataGridView1.Columns["hourlyPayRateDataGridViewTextBoxColumn"], System.ComponentModel.ListSortDirection.Descending);
+        }
+    }
+}
